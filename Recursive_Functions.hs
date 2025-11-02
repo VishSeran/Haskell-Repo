b764d8a0dea0c4f19f4513ed0d922a8d1bc22bc1
VishSeran <vishwa.sovis@gmail.com>
2025-11-02 15:27:10 +0530

    exercise 1 updated

--- Recursive_Functions.hs
+++ Recursive_Functions.hs
@@ -4,11 +4,8 @@
 {-# HLINT ignore "Redundant bracket" #-}
 import Prelude hiding (and)
 
-and :: [Bool] -> Bool
-and [] = True
-and (x : xs) = x && and xs
 
-main:: IO()
-main = do
-    let list1 = [True, True, False, True]
-    putStrLn $ "output of and function: " ++ show (and (list1));
+and :: [a] -> Bool
+and [] = True
+
+
